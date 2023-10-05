1cb430f3c1c643c8a7fc75c47d827381c50a3aa0
Ezekiel Enns <Ezekiel.Enns@protonmail.com>
2023-10-05 15:26:15 -0300

    gottem good, neovim config is now added to the mix for finer

--- scripts/finder.sh
+++ scripts/finder.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash 
 set -x
-wp=`find ~/Documents/ -maxdepth 3 -type f -name 'shell.nix' |  fzf` 
-( cd $(dirname $wp) ; nix-shell --command "kitty --detach --session ~/Documents/configs/kitty/work.conf" ) 
+wp=`find ~/Documents/ -maxdepth 3 -type f -name 'default.nix' |  fzf` || exit
+( cd $(dirname $wp) ; nix develop github:EzekielEnns/editor --command bash -c "nix-shell --command 'kitty --detach --session ~/Documents/configs/kitty/work.conf'" ) 
