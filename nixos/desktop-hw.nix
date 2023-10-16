f2f2782885357f76265692a38cb7bc92260c7362
ezekielenns <ezekiel.enns@protonmail.com>
2023-10-16 08:15:19 -0300

    added tp link to dk

--- nixos/desktop-hw.nix
+++ nixos/desktop-hw.nix
@@ -33,6 +33,7 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
