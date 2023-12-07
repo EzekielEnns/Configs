ed64d68ce5f3af22a017ee7615bf75f6c1ba97f2
ezekielenns <ezekiel.enns@protonmail.com>
2023-12-07 08:28:41 -0400

    removed comic reader

--- nixos/general.nix
+++ nixos/general.nix
@@ -185,8 +185,6 @@ in {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #TODO move libresprite
-    komikku
     jmtpfs
     veikk_driver
     finder
