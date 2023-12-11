8904e996fffbca2d7e711df2be21f710ab64ea6b
Ezekiel Enns <Ezekiel.Enns@protonmail.com>
2023-12-11 09:12:31 -0400

    added helix

--- nixos/general.nix
+++ nixos/general.nix
@@ -185,6 +185,7 @@ in {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    helix
     jmtpfs
     veikk_driver
     finder
