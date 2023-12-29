d0c89bd888d2b8decaca4760f5e70327bd7a0ef3
ezekielenns <ezekiel.enns@protonmail.com>
2023-12-29 10:07:27 -0400

    fixing a bug....

--- scripts/finder.sh
+++ scripts/finder.sh
@@ -1,4 +1,4 @@
 set -x
 wp=`find ~/Documents/ -maxdepth 3 -type f -name 'default.nix' |  fzf` || exit
-( cd $(dirname $wp) ; nix develop github:EzekielEnns/editor \
+( cd $(dirname $wp) ; nix develop github:EzekielEnns/editor --refresh --impure \
     --command bash -c "nix-shell --command 'kitty --detach --session ~/.config/kitty/work.conf'" ) 
