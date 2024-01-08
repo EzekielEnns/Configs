6570092237fa2f406f21dec251c6d723d121851e
Ezekiel Enns <Ezekiel.Enns@protonmail.com>
2024-01-08 08:33:01 -0400

    finally fixed finder script... why is terraform 'not free' now

--- scripts/finder.sh
+++ scripts/finder.sh
@@ -1,3 +1,4 @@
 set -x
 wp=`find ~/Documents/ -maxdepth 3 -type f -name 'default.nix' |  fzf` || exit
-( cd $(dirname $wp) ; nix-shell --command 'kitty --detach --session ~/.config/kitty/work.conf' ) 
+( cd $(dirname $wp) ; NIXPKGS_ALLOW_UNFREE=1 nix-shell --command 'kitty --detach --session ~/.config/kitty/work.conf' )
+#sleep 1
