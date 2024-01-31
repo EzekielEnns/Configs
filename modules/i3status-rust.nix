01c318d2ccab8be5e5ad60ada23da54c0ec1c41a
Ezekiel Enns <Ezekiel.Enns@protonmail.com>
2024-01-31 15:56:40 -0400

    lets goooooo

--- modules/i3status-rust.nix
+++ modules/i3status-rust.nix
@@ -21,11 +21,11 @@
                             info_type = "used";
                             format = " $used";
                         }
-                        {
-                            block = "load";
-                            interval = 5;
-                            icons_format = "{icon} $m";
-                        }
+                        # {
+                        #     block = "load";
+                        #     interval = 5;
+                        #     icons_format = "{icon} ";
+                        # }
                         {
                             block = "memory";
                             format = " $icon  $mem_used_percents ";
