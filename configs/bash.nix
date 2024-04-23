2ec0d9ef5a369c12a6e16a8559d394a18a181b75
Ezekiel Enns <Ezekiel.Enns@protonmail.com>
2024-04-22 23:00:04 -0300

    forgot some stuff

--- configs/bash.nix
+++ configs/bash.nix
@@ -5,9 +5,6 @@
         environment.etc.bashrc.text = builtins.readFile(../misc/.bashrc);
         environment.etc.inputrc.text = builtins.readFile(../misc/.inputrc);
         programs.bash.shellAliases = {
-            #this keeps my editor always up-todate so i dont need to update
-            #my whole os everytime i change my editor
-            nvim = "nix develop github:ezekielenns/editor --command nvim";
         };
     };
 }
