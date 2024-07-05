203047bf54c01730e311889c2fd834fc7ada53e7
ezekielenns <ezekiel.enns@protonmail.com>
2024-07-05 12:16:39 -0600

    added alias

--- configs/bash.nix
+++ configs/bash.nix
@@ -5,6 +5,7 @@
         environment.etc.bashrc.text = builtins.readFile(../misc/.bashrc);
         environment.etc.inputrc.text = builtins.readFile(../misc/.inputrc);
         programs.bash.shellAliases = {
+            yt-dl-audio = "yt-dlp --ignore-errors --output '%(title)s.%(ext)s' --extract-audio --audio-format wav";
         };
     };
 }
