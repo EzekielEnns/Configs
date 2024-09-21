d6de5a1153865b1127116ba05dc9b56263f0dd70
ezekielenns <ezekiel.enns@protonmail.com>
2024-09-21 13:05:52 -0600

    fresh install

--- nixos/dk.nix
+++ nixos/dk.nix
@@ -29,9 +29,8 @@
     # Enable the Nvidia settings menu,
 	# accessible via `nvidia-settings`.
     nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    #https://nixos.wiki/wiki/Nvidia
+    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 
   services.nginx = {
