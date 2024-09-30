7dd42318e4914867bc38a74b9640f83194e32997
Zeke <ezekiel.enns@protonmail.com>
2024-09-30 08:26:52 -0600

    removed that bell

--- configs/kitty.nix
+++ configs/kitty.nix
@@ -16,30 +16,15 @@
                 "clear_all_shortcuts"= "yes";
                 "show_hyperlink_targets"="yes";
                 "copy_on_select"="clipboard";
-                "enable_audio_bell"="yes";
                 "tab_bar_style"="hidden";
                 "window_alert_on_bell" = "yes";
-                #todo add a sound
-                #"bell_path none" 
-                #https://sw.kovidgoyal.net/kitty/conf/#terminal-bell
+                "enable_audio_bell" =  "no";
             };
             keybindings = {
                 "ctrl+shift+equal"="change_font_size all +2.0";
                 "ctrl+shift+minus"="change_font_size all -2.0";
                 "cmd+minus"="change_font_size all -2.0 ";
                 "ctrl+shift+v"="paste_from_clipboard";
-                # "ctrl+b>c"="new_tab_with_cwd";
-                # "ctrl+b>%"="new_window";
-                # "ctrl+b>w"="select_tab";
-                # "ctrl+b>"="next_layout";
-                # "ctrl+b>s"="swap_with_window";
-                # "ctrl+b>r"="set_tab_title";
-                # #"ctrl+b>space"="focus_visible_window";
-                # "ctrl+b>n"="start_resizing_window";
-                # "alt+tab"="next_tab";
-                # "ctrl+shift+tab"="previous_tab";
-                # "ctrl+tab"="focus_visible_window";
-                # "alt+shift+tab"="previous_window";
             };
         };
     };
