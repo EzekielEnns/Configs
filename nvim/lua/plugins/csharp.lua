530be5c2902be4293056d7d0427a6a79cca03fd8
Zeke <ezekiel.enns@protonmail.com>
2025-07-11 11:11:22 -0600

    adding csharp cause why not

---
+++ nvim/lua/plugins/csharp.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"iabdelkareem/csharp.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-dap",
+			"Tastyep/structlog.nvim",
+		},
+		config = function()
+			require("csharp").setup({
+				lsp = {
+					-- Set to false if you want to disable automatic setup of omnisharp
+					enable = true,
+					-- Set to true if you want to use roslyn instead of omnisharp
+					omnisharp = {
+						enable = true,
+						-- Set to true if you want to use mono version of omnisharp
+						use_mono = false,
+					},
+				},
+				dap = {
+					-- Set to false if you want to disable automatic setup of netcoredbg
+					enable = true,
+				},
+			})
+		end,
+	},
+}
