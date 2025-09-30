0d274610e2b933e184dd69f43efc249e192d52d5
Zeke <ezekiel.enns@protonmail.com>
2025-09-30 11:26:26 -0600

    lsd babyyyy

--- modules/shell.nix
+++ modules/shell.nix
@@ -1,31 +1,32 @@
-{pkgs, ...}: 
+{ pkgs, ... }:
 {
-    options = {};
-    config = {
-        environment.systemPackages = with pkgs; [
-                fzf
-                neofetch 
-                ripgrep
-                fzf-git-sh
-                bat
-                bashInteractive
-        ];
-        environment.variables = {
-            EDITOR = "nvim";
-            FZF_COMPLETION_OPTS="--border --info=inline";
-            VISUAL = "nvim";
-            NIXPKGS_ALLOW_UNFREE = "1";
-            FZF_CTRL_T_OPTS="--walker-skip .git,node_modules,target --preview 'bat -n --color=always {}' --bind 'ctrl-/:change-preview-window(down|hidden|)'";
-        };
-        programs.zsh = {
-            enable = true;
-            enableCompletion=true;
-            promptInit = ''
-                bindkey -v
-                eval "$(starship init zsh)"
-                eval "$(direnv hook zsh)"
-                eval "$(zoxide init zsh)"
-                '';
-        };
+  options = { };
+  config = {
+    environment.systemPackages = with pkgs; [
+      fzf
+      neofetch
+      ripgrep
+      fzf-git-sh
+      bat
+      bashInteractive
+      lsd
+    ];
+    environment.variables = {
+      EDITOR = "nvim";
+      FZF_COMPLETION_OPTS = "--border --info=inline";
+      VISUAL = "nvim";
+      NIXPKGS_ALLOW_UNFREE = "1";
+      FZF_CTRL_T_OPTS = "--walker-skip .git,node_modules,target --preview 'bat -n --color=always {}' --bind 'ctrl-/:change-preview-window(down|hidden|)'";
     };
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      promptInit = ''
+        bindkey -v
+        eval "$(starship init zsh)"
+        eval "$(direnv hook zsh)"
+        eval "$(zoxide init zsh)"
+      '';
+    };
+  };
 }
