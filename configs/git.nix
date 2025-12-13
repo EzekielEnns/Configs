e80ecaf1765456134e89ea369ebce30a8b081158
Zeke <ezekiel.enns@protonmail.com>
2025-12-13 10:55:30 -0700

    gotter done

--- configs/git.nix
+++ configs/git.nix
@@ -3,13 +3,14 @@
     options = {};
     config = {
         programs.git = {
-        userName  = "Zeke";
-        userEmail = "ezekiel.enns@protonmail.com";
         enable = true;
-        aliases = {
-            capri-sun ="!f() { if [ \"\$1\" = \"-a\" ]; then git commit -am \"\$(git rev-parse --abbrev-ref HEAD) \$2\"; else git commit -m \"\$(git rev-parse --abbrev-ref HEAD) \$1\"; fi; }; f";
-        };
-        extraConfig = {
+        settings = {
+            user = {
+                name  = "Zeke";
+                email = "ezekiel.enns@protonmail.com";
+            };
+
+            alias.capri-sun = "!f() { if [ \"\$1\" = \"-a\" ]; then git commit -am \"\$(git rev-parse --abbrev-ref HEAD) \$2\"; else git commit -m \"\$(git rev-parse --abbrev-ref HEAD) \$1\"; fi; }; f";
 
             init.defaultBranch = "main";
             mergetool.nvimdiff.cmd = "nvim -d -c \"wincmd l\" -c \"norm ]c\" \"$LOCAL\" \"$MERGED\" \"$REMOTE\"";
--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-darwin = {
-      url = "github:lnl7/nix-darwin/nix-darwin-25.05";
+      url = "github:lnl7/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     ghostty = {
